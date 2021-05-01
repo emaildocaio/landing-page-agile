2370bad1feb9e4652d6e8cfa03a5ed3cd13aed91
Caio Farias <emaildocaio@gmail.com>
2021-05-01 20:15:09 -0300

    OK

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ function myFunction() {
     }
   }
 
-  const phoneInputField = document.querySelector("#phone");
-   const phoneInput = window.intlTelInput(phoneInputField, {
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
     preferredCountries: ["br", "us", "ca", "fr"],
      utilsScript:
        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
    });
 
+   function obrigado() {
+    const nome = document.querySelector('#nome').value;
+    console.log(nome)
+   };
 
+   
 
-var li = document.getElementById("li-produto")
-console.log(li)
